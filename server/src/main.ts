8f46059b4dabf58b4c49b642ca455744b517c311
surguh <surguh@yandex.ru>
2022-04-18 15:58:19 +0300

    feat: настройка prettier

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule, new FastifyAdapter({ logger: true })
+    AppModule,
+    new FastifyAdapter({ logger: true })
   )
   await app.listen(3000)
 }
